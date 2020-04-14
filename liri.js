d92743755b7c71c394d52d8f5bacabfb6dbbfe57
KAT <katreadwell1@gmail.com>
2020-04-13 19:28:18 -0700

    fixed outstanding problems, need to clean up random

--- liri.js
+++ liri.js
@@ -2,59 +2,148 @@ if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
 }
 
-var command = process.argv[2];
-var search = process.argv.slice(3).join(" ");
-
-function directory(){
-    if(command === "concert-this"){
-        eventLookup(search);
-    }
-//add 2 more if and 1 else if
-}
-
-//will it return exports.spotify?  what is in {}? ID + Secret
-
-//concert-this
-//I ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
-//app_id = codingbootcamp
-
-
-function eventLookup(artist){
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id+codingbootcamp";
-    console.log(artist);
-    console.log()
-}
-
-directory();
-
-
-
-
-
-//spotify-this-song
+var command1 = process.argv[2];
+var search2 = process.argv.slice(3).join(" ");
+var axios = require("axios");
+var fs = require("fs");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify({
     id: process.env.SPOTIFY_ID,
     secret: process.env.SPOTIFY_SECRET
-});
-console.log(`Created Spotify client with ${process.env.SPOTIFY_ID}, ${process.env.SPOTIFY_SECRET}.`)
+})
+// console.log(`Created Spotify client with ${process.env.SPOTIFY_ID}, ${process.env.SPOTIFY_SECRET}.`)
 
-// var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret>
-// });
- 
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
+
+function directory(command, search) {
+    if (command === "concert-this") {
+        eventLookup(search);
+    }
+    else if (command === "spotify-this-song") {
+        songLookup(search);
+    }
+    else if (command === "movie-this") {
+        movieLookup(search);
+    }
+    else{
+        random();
+    }
+}
+
+const convertDate = (date, format) => {
+    let today = new Date(date);
+    let dd = today.getDate() + 1,
+        mm = today.getMonth() + 1,
+        yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    today = mm + '/' + dd + '/' + yyyy;
+    return today;
+}
+
+//bands in town
+function eventLookup(search) {
+    axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            //need to return all results and format datatime in MM/DD/YYY
+            for (var i = 0; i < response.data.length; i++) {
+                console.log("The venue is " + response.data[i].venue.name);
+                console.log("The band will be playing in " + response.data[i].venue.city);
+                console.log("On " + convertDate(response.data[i].datetime));
+            }
+        })
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+}
+
+
+
+
+//spotify this song
+//if no song provided, need to return "The Sign" by Ace of Base
+function songLookup(search) {
+    console.log('spotify song: ', search);
+    spotify.search({ type: 'track', query: search, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var songResult = data.tracks.items[0];
+        console.log(songResult.artists[0].name);
+        console.log(songResult.name);
+        console.log(songResult.preview_url);
+        console.log(songResult.album.name);
+    });
+
+    
+    //     console.log(response.data);
+    //     console.log("Artist Name: " + response.data);
+    //     console.log("Song Name: " + response.data);
+    //     console.log("Preview Link: " + response.data);
+    //     console.log("Album: " + response.data);
+    
+
+}
 
 
 //movie-this
-//axios package api key=trilogy
+//stuck on how to display rotten tomato score
+//if user doesn't type in movie, it defaults to Mr.Nobody
+function movieLookup(search) {
+    axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }
+    )
+        .catch(function (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+};
+
 
 //do-what-it-says
-//fs nod pkg
+//run spotify-this-song for "I Want it That Way" from random.txt, can also replace text for movie-this and concert-this
+function random() {
+    fs.readFile("random.txt", "utf8", (err, data) => {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        var dataArr = data.split(",")
+        console.log(dataArr);
+        directory(dataArr[0], dataArr[1]);
+        })
+};
+
+directory(command1, search2);
+
+
+
